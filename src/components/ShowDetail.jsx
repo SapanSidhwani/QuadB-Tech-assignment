711a2a6e427dca9278ba5fd46a92dd81ed2deb3c
Sapan <sapansidhwani40@gmail.com>
2024-02-01 00:44:36 +0530

    basic issues

--- src/components/ShowDetail.jsx
+++ src/components/ShowDetail.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import { Link, useParams } from 'react-router-dom';
 import { useState, useEffect } from 'react';
-import noImage from '../assets/no-image.png'
 
 const ShowDetail = () => {
 
